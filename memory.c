2892f140399f20783891775cd82c0f26783ad164
Lionel Flandrin <lionel@svkt.org>
2019-07-17 10:47:01 +0200

    Implement zero-page RAM

--- memory.c
+++ memory.c
@@ -4,6 +4,9 @@
 /* ROM (bank 0 + 1) */
 #define ROM_BASE        0x0000U
 #define ROM_END         (ROM_BASE + 0x8000U)
+/* Zero page RAM */
+#define ZRAM_BASE       0xff80U
+#define ZRAM_END        (ZRAM_BASE + 0x7fU)
 
 /* Read one byte from memory at `addr` */
 uint8_t gb_memory_readb(struct gb *gb, uint16_t addr) {
@@ -18,6 +21,12 @@ uint8_t gb_memory_readb(struct gb *gb, uint16_t addr) {
 }
 
 void gb_memory_writeb(struct gb *gb, uint16_t addr, uint8_t val) {
+
+     if (addr >= ZRAM_BASE && addr < ZRAM_END) {
+          gb->zram[addr - ZRAM_BASE] = val;
+          return;
+     }
+
      printf("Unsupported write at address 0x%04x [val=0x%02x]\n", addr, val);
      die();
 }
