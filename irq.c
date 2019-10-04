8e72454ec7c9bd77fead656fa43bcb15f09b8515
Lionel Flandrin <lionel@svkt.org>
2019-10-04 12:53:27 +0100

    Implement IRQ enable register

--- irq.c
+++ irq.c
@@ -4,6 +4,7 @@ void gb_irq_reset(struct gb *gb) {
      struct gb_irq *irq = &gb->irq;
 
      irq->irq_flags = 0xE0;
+     irq->irq_enable = 0;
 }
 
 void gb_irq_trigger(struct gb *gb, enum gb_irq_token which) {
