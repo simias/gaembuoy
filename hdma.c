ca215229c3172b5456f5c37eca1ed3fa5355aa1e
Lionel Flandrin <lionel@svkt.org>
2019-10-23 15:08:55 +0100

    Basic HDMA support

---
+++ hdma.c
@@ -0,0 +1,46 @@
+#include "gb.h"
+
+static void gb_hdma_copy(struct gb *gb, uint16_t len) {
+     struct gb_hdma *hdma = &gb->hdma;
+     uint16_t src = hdma->source;
+     uint16_t dst = hdma->destination;
+
+     while (len--) {
+          uint16_t vram_addr;
+          uint8_t v;
+
+          /* Destination has to be in VRAM */
+          vram_addr = 0x8000U + (dst % 0x2000U);
+
+          v = gb_memory_readb(gb, src);
+          gb_memory_writeb(gb, vram_addr, v);
+
+          src++;
+          dst++;
+     }
+
+     hdma->source = src;
+     hdma->destination = dst;
+}
+
+void gb_hdma_start(struct gb *gb, bool hblank) {
+     struct gb_hdma *hdma = &gb->hdma;
+
+     /* XXX TODO: for now assume we always run at once */
+     hblank = false;
+
+     if (hblank) {
+          /* TODO */
+     } else {
+          /* Do the transfer in one shot */
+          uint16_t len = hdma->length;
+
+          len = (len + 1) * 0x10;
+
+          gb_hdma_copy(gb, len);
+
+          /* Transfer done */
+          hdma->run_on_hblank = false;
+          hdma->length = 0x7f;
+     }
+}
---
+++ hdma.h
@@ -0,0 +1,20 @@
+#ifndef _GB_HDMA_H_
+#define _GB_HDMA_H_
+
+struct gb_hdma {
+     /* Source address */
+     uint16_t source;
+     /* Destination offset in VRAM */
+     uint16_t destination;
+     /* Remaining length to copy, divided by 0x10 and decremented (so a value of
+      * 0 means 0x10 bytes left, a value of 0x42 means (0x42 + 1) * 0x10 -> 670
+      * bytes) */
+     uint8_t length;
+     /* True if we currently transfer 0x10 bytes at a time during the horizontal
+      * blanking */
+     bool run_on_hblank;
+};
+
+void gb_hdma_start(struct gb *gb, bool hblank);
+
+#endif /* _GB_HDMA_H_ */
